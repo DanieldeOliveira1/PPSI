7395ae4df0ed1cbfc123a52421d3ea17589372bf
Daniel de Oliveira <daniel_santos@unigranrio.br>
2020-10-05 13:17:57 -0300

    Add files via upload

---
+++ Atividade PPSI/atividade4.py
@@ -0,0 +1,16 @@
+"""
+Faça um programa que leia dois números e mostre qual o maior dos dois.
+O programa deve informar caso sejam iguais.
+"""
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+
+if a > b:
+    print('O primeiro valor é maior')
+    
+elif b > a:
+    print('O segundo valor é maior')
+    
+else:
+    print('Os dois valores são iguais')
